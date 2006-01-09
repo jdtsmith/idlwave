d1dbf77a1c2c1058c8f8ffb81884eb5e22aa70a8
jdsmith <jdsmith>
2006-01-09 19:36:35 +0000

    - Updated FSF address.

--- idlw-complete-structtag.el
+++ idlw-complete-structtag.el
@@ -4,7 +4,7 @@
 ;; Author: Carsten Dominik <dominik@astro.uva.nl>
 ;; Maintainer: J.D. Smith <jdsmith@as.arizona.edu>
 ;; Version: 1.2
-;; Date: $Date: 2005/07/04 01:29:41 $
+;; Date: $Date: 2006/01/09 19:36:56 $
 ;; Keywords: languages
 
 ;; This file is part of GNU Emacs.
@@ -21,8 +21,8 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
